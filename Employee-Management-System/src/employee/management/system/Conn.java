86103dfae6f7577eb8017d7f73d1c7a8e6c02618
sharadchauhan123 <142665778+sharadchauhan123@users.noreply.github.com>
2023-11-03 15:07:08 +0530

    first commit

---
+++ Employee-Management-System/src/employee/management/system/Conn.java
@@ -0,0 +1,24 @@
+package employee.management.system;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+
+    Connection connection;
+    Statement statement;
+
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem","root","1234");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
